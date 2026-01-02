7d011e8979ae5d1a811a58580f0d1c7b2c6c02d5
spades02 <01-131202-036@student.bahria.edu.pk>
2026-01-02 17:37:47 +0500

    add loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import NavBar from "@/components/nav-bar";
+import NextTopLoader from 'nextjs-toploader';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,7 +39,16 @@ export default async function RootLayout({
         {/* {!session && <GuestNavbar/>}
         {session && <NavBar/>} */}
           <NavBar />
-        <main>{children}</main>
+        <main>
+        <NextTopLoader 
+          color="#2299DD" 
+          initialPosition={0.08} 
+          crawlSpeed={200} 
+          height={3} 
+          showSpinner={false} 
+        />
+        {children}
+        </main>
         {/* Toaster can also be a client component inside */}
         <Toaster />
       </body>
